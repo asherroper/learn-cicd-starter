1f0e7e3a90885438d393e058cc2066e33b7340e9
asherroper <asherhroper@gmail.com>
2026-02-27 10:44:26 -0800

    added tests for TestGetAPIKey(). Also updated ci.yml to run all tests

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,54 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	input1 := make(http.Header)
+	input1.Set("Authorization", "ApiKey testtok")
+	input2 := make(http.Header)
+	input2.Set("Authorization", "testtok")
+	input3 := make(http.Header)
+
+	tests := map[string]struct {
+		input   http.Header
+		want    string
+		wantErr error
+	}{
+		"contains auth": {
+			input:   input1,
+			want:    "testtok",
+			wantErr: nil,
+		},
+		"malformed header": {
+			input: input2,
+			want:  "",
+			// wantErr: ErrMalformedAuthHeader,
+			wantErr: errors.New("malformed authorization header"),
+		},
+		"missing auth": {
+			input:   input3,
+			want:    "",
+			wantErr: ErrNoAuthHeaderIncluded,
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			got, err := GetAPIKey(tc.input)
+
+			// unexpected err
+			if !errors.Is(err, tc.wantErr) {
+				t.Errorf("GetAPIKey() error = %v, wantErr %v", err, tc.wantErr)
+			}
+
+			// unexpected got
+			if tc.wantErr == nil && got != tc.want {
+				t.Errorf("GetAPIKey() got = %v, want %v", got, tc.want)
+			}
+		})
+	}
+}
